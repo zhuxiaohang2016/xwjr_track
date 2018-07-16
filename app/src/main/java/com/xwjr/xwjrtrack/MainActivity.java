f74d46e04abab9d891adaecab121644238bccbae
朱小航 <zhuxh@newhope.cn>
2018-07-16 09:33:47 +0800

    修改配置

--- app/src/main/java/com/xwjr/xwjrtrack/MainActivity.java
+++ app/src/main/java/com/xwjr/xwjrtrack/MainActivity.java
@@ -3,6 +3,8 @@ package com.xwjr.xwjrtrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.xwjr.track.TrackConfig;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override

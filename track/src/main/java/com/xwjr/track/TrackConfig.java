e531547a1d9639240b1dca9a6d09acceb50723e2
朱小航 <zhuxh@newhope.cn>
2018-08-27 14:15:50 +0800

    修改某些属性权限

--- track/src/main/java/com/xwjr/track/TrackConfig.java
+++ track/src/main/java/com/xwjr/track/TrackConfig.java
@@ -6,17 +6,17 @@ import com.amap.api.location.AMapLocationClient;
 
 public class TrackConfig {
 
-    public static Context context;
-    public static String logTag = "track";
-    public static String trackUrl = "http://p2psp.kfxfd.cn:9080/apphub/tracking/";
-    public static String trackApphubkey = "49dd08f0-24e6-11e7-b026-6b0b8b32be51";
-    public static String latitude = "";
-    public static String longitude = "";
-    public static String address = "";
-    public static int singleDataLimit = 20;
-    public static long locationInterval = 60000;
-    public static boolean localDataAutoUpload = true;
-    public static boolean debug = false;
+    static Context context;
+    static String logTag = "track";
+    static String trackUrl = "http://p2psp.kfxfd.cn:9080/apphub/tracking/";
+    static String trackApphubkey = "49dd08f0-24e6-11e7-b026-6b0b8b32be51";
+    static String latitude = "";
+    static String longitude = "";
+    static String address = "";
+    static int singleDataLimit = 20;
+    static long locationInterval = 60000;
+    static boolean localDataAutoUpload = true;
+    static boolean debug = false;
 
     public static void init(Context context, String trackUrl, String trackApphubkey, String amapKey) {
         try {
@@ -53,6 +53,7 @@ public class TrackConfig {
     public static void setLocalDataAutoUpload(boolean data) {
         localDataAutoUpload = data;
     }
+
     //设置debug模式
     public static void setDebug(boolean data) {
         debug = data;

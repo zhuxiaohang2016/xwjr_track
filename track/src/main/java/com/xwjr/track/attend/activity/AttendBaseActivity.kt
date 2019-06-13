2a933c12a0f3d946f0dcc783341725b9e05fd341
朱小航 <zhuxh@newhope.cn>
2019-06-13 18:51:55 +0800

    调整statusbar 显示

--- track/src/main/java/com/xwjr/track/attend/activity/AttendBaseActivity.kt
+++ track/src/main/java/com/xwjr/track/attend/activity/AttendBaseActivity.kt
@@ -1,14 +1,13 @@
 package com.xwjr.track.attend.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import com.gyf.immersionbar.ImmersionBar
 import com.xwjr.track.R
 
 open class AttendBaseActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         ImmersionBar.with(this)
                 .statusBarColor(R.color.white)
                 .autoStatusBarDarkModeEnable(true)
